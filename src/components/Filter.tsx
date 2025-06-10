87b5dc842f202e1d86c5ba9fd93f22aa087bea05
Majd-gh <mghattas545@gmail.com>
2025-06-10 23:36:08 +0300

    edite type of level

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -7,7 +7,7 @@ const Filter = ({setLevel} : filterProps) => {
     return (
       <div className="filter">
         <div>Filter By Level</div>
-        <select onChange={(e)=>{setLevel(e.target.value)}}>
+        <select onChange={(e)=>{setLevel(Number(e.target.value))}}>
           <option value={0}>All Level</option>
           <option value={1}>Level 1</option>
           <option value={2}>Level 2</option>
